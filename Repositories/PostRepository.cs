753a238becc32f1a3e142ee32b19ae22d8ef0cc0
Jakub <jakub.surdej@codahead.com>
2022-12-17 12:40:30 +0100

    Simplify repositories using inheritance

--- Repositories/PostRepository.cs
+++ Repositories/PostRepository.cs
@@ -4,57 +4,8 @@ using SimpleBlog.Repositories.Interfaces;
 
 namespace SimpleBlog.Repositories
 {
-    public class PostRepository : IPostRepository
+    public class PostRepository : BaseRepository<Post>, IPostRepository
     {
-        private readonly BlogContext _blogContext;
-
-        public PostRepository(BlogContext ctx)
-        {
-            _blogContext = ctx;
-        }
-
-        public List<Post> GetAll()
-        {
-            return _blogContext.Posts!.ToList();
-        }
-
-        public Post? Get(int id)
-        {
-            return _blogContext.Posts!.Find(id);
-        }
-
-        public Post Delete(int id)
-        {
-            var dbPost = _blogContext.Posts!.Find(id);
-            if (dbPost == null)
-            {
-                throw new ArgumentException($"Could not find post with id ${id}");
-            }
-
-            _blogContext.Remove(dbPost);
-            _blogContext.SaveChanges();
-
-            return dbPost;
-        }
-
-        public Post Create(Post post)
-        {
-            _blogContext.Posts!.Add(post);
-            _blogContext.SaveChanges();
-            return post;
-        }
-
-        public Post Modify(Post newPost)
-        {
-            var oldPost = _blogContext.Posts!.Find(newPost);
-            if (oldPost == null)
-            {
-                throw new ArgumentException("Could not find post with given id");
-            }
-
-            _blogContext.Entry(oldPost).CurrentValues.SetValues(newPost);
-            _blogContext.SaveChanges();
-            return newPost;
-        }
+        public PostRepository(BlogContext ctx) : base(ctx, ctx.Posts!) { }
     }
 }
--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -4,62 +4,13 @@ using SimpleBlog.Repositories.Interfaces;
 
 namespace SimpleBlog.Repositories
 {
-    public class UserRepository : IUserRepository
+    public class UserRepository : BaseRepository<User>, IUserRepository
     {
-        private readonly BlogContext _blogContext;
-
-        public UserRepository(BlogContext ctx)
-        {
-            _blogContext = ctx;
-        }
-
-        public List<User> GetAll()
-        {
-            return _blogContext.Users!.ToList();
-        }
-
-        public User? Get(int id)
-        {
-            return _blogContext.Users!.Find(id);
-        }
+        public UserRepository(BlogContext ctx) : base(ctx, ctx.Users!) { }
 
         public User? GetByEmail(string email)
         {
-            return _blogContext.Users!.Find(new User { Email = email });
-        }
-
-        public User Delete(int id)
-        {
-            var dbUser = _blogContext.Users!.Find(id);
-            if (dbUser == null)
-            {
-                throw new ArgumentException($"Could not find user with id ${id}");
-            }
-
-            _blogContext.Remove(dbUser);
-            _blogContext.SaveChanges();
-
-            return dbUser;
-        }
-
-        public User Create(User user)
-        {
-            _blogContext.Users!.Add(user);
-            _blogContext.SaveChanges();
-            return user;
-        }
-
-        public User Modify(User newUser)
-        {
-            var oldUser = _blogContext.Users!.Find(newUser);
-            if (oldUser == null)
-            {
-                throw new ArgumentException("Could not find user with given id");
-            }
-
-            _blogContext.Entry(oldUser).CurrentValues.SetValues(newUser);
-            _blogContext.SaveChanges();
-            return newUser;
+            return _dbSet.Find(new User { Email = email });
         }
     }
 }
