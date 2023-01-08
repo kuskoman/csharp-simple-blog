024905012e2d49c72b8e322e581132b8b460f5f8
Jakub Surdej <jakub.surdej@hemfrid.se>
2023-01-08 10:26:49 +0100

    Disable password validation

--- Utils/AuthSetup.cs
+++ Utils/AuthSetup.cs
@@ -13,7 +13,13 @@ namespace SimpleBlog.Utils
             builder.Services
                 .AddIdentity<User, Role>(options =>
                 {
-                    options.Password.RequiredLength = 8;
+                    options.Password.RequiredLength = 7;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequiredUniqueChars = 0;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                    options.Password.RequireUppercase = false;
+
                     options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(1.0);
                     options.Lockout.MaxFailedAccessAttempts = 5;
                     options.User.RequireUniqueEmail = true;
