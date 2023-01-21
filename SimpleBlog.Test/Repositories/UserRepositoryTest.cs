b4a281b8e127b2e8fd52db34a41a816cdda488e2
Jakub <jakub.surdej@codahead.com>
2023-01-21 11:55:15 +0100

    Add more tests (and find some bugs)

--- SimpleBlog.Test/Repositories/UserRepositoryTest.cs
+++ SimpleBlog.Test/Repositories/UserRepositoryTest.cs
@@ -33,7 +33,7 @@ namespace SimpleBlog.Test.Repositories
         }
 
         [Fact]
-        public void TestGet()
+        public void TestGetUserExists()
         {
             var choosenOne = new User() { Name = "Choosen one", Id = 2 };
             var users = new List<User>()
@@ -48,12 +48,108 @@ namespace SimpleBlog.Test.Repositories
             Assert.Equal(_repository.Get(choosenOne.Id), choosenOne);
         }
 
+        [Fact]
+        public void TestGetUserNotExist()
+        {
+            var users = new List<User>() { new User() { Id = 1 } };
+
+            CreateUsers(users);
+
+            Assert.Null(_repository.Get(99));
+        }
+
         private void CreateUsers(List<User> users)
         {
             foreach (var user in users)
             {
                 _repository.Create(user);
             }
+        }
+
+        [Fact]
+        public void TestDeleteUserExists()
+        {
+            var userToDelete = new User() { Id = 2 };
+            var users = new List<User>()
+            {
+                new User() { Id = 1 },
+                userToDelete,
+                new User() { Id = 3 }
+            };
+
+            CreateUsers(users);
+
+            Assert.Equal(_repository.Delete(userToDelete.Id), userToDelete);
+            Assert.DoesNotContain(userToDelete, _repository.GetAll());
+        }
+
+        [Fact]
+        public void TestDeleteUserNotExist()
+        {
+            var users = new List<User>() { new User() { Id = 1 } };
+
+            CreateUsers(users);
+
+            Assert.Throws<ArgumentException>(() => _repository.Delete(99));
+        }
+
+        [Fact]
+        public void TestCreateUser()
+        {
+            var newUser = new User() { Id = 2 };
+            _repository.Create(newUser);
+
+            Assert.Contains(newUser, _repository.GetAll());
+        }
+
+        [Fact]
+        public void TestModifyUserExists()
+        {
+            var userToModify = new User() { Id = 2, Name = "Original name" };
+            var modifiedUser = new User() { Id = 2, Name = "Modified name" };
+            var users = new List<User>()
+            {
+                new User() { Id = 1 },
+                userToModify
+            };
+
+            CreateUsers(users);
+
+            Assert.Equal(_repository.Modify(modifiedUser), modifiedUser);
+            Assert.Equal(_repository.Get(userToModify.Id), modifiedUser);
+        }
+
+        [Fact]
+        public void TestModifyUserNotExist()
+        {
+            var userToModify = new User() { Id = 99, Name = "Modified name" };
+
+            Assert.Throws<ArgumentException>(() => _repository.Modify(userToModify));
+        }
+
+        [Fact]
+        public void TestGetByEmailExist()
+        {
+            var user = new User() { Email = "test@example.com", Id = 2 };
+            var users = new List<User>()
+            {
+                new User() { Id = 1 },
+                user
+            };
+
+            CreateUsers(users);
+
+            Assert.Equal(_repository.GetByEmail(user.Email), user);
+        }
+
+        [Fact]
+        public void TestGetByEmailNotExist()
+        {
+            var users = new List<User>() { new User() { Id = 1 } };
+
+            CreateUsers(users);
+
+            Assert.Null(_repository.GetByEmail("nonexistent@example.com"));
         }
     }
 }
