e06e66497a7dde3ab8474f005048e195deb04b4d
Jakub <jakub.surdej@codahead.com>
2023-01-22 23:01:27 +0100

    Improve Post component

---
+++ SimpleBlog.Web/ClientApp/src/components/Blog/Author.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card-item class="author-item">
+    <span class="author-label">Author: </span>
+    <span class="author-name">{{ author }}</span>
+  </v-card-item>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BlogAuthor",
+  props: {
+    author: String,
+  },
+});
+</script>
+
+<style scoped lang="scss">
+.author-item {
+  font: {
+    size: 0.8em;
+  }
+}
+</style>
--- SimpleBlog.Web/ClientApp/src/components/Blog/Post.vue
+++ SimpleBlog.Web/ClientApp/src/components/Blog/Post.vue
@@ -1,14 +1,14 @@
 <template>
-  <v-card>
-    <article>
-      <h2>{{ title }}</h2>
-      <p>{{ body }}</p>
-      <span class="author">{{ author }}</span>
-    </article>
+  <v-card tag="article">
+    <v-card-title tag="h2">{{ title }}</v-card-title>
+    <v-card-text tag="p">{{ body }}</v-card-text>
+    <blog-author :author="author"></blog-author>
   </v-card>
 </template>
 
 <script lang="ts">
+import BlogAuthor from "./Author.vue";
+
 import { defineComponent } from "vue";
 
 export default defineComponent({
@@ -18,7 +18,32 @@ export default defineComponent({
     body: String,
     author: String,
   },
+  components: {
+    BlogAuthor,
+  },
 });
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+article {
+  padding: {
+    top: 20px;
+    bottom: 20px;
+    left: 40px;
+    right: 40px;
+  }
+  margin: {
+    top: 20px;
+    bottom: 20px;
+  }
+  border: {
+    radius: 0;
+  }
+
+  .author-item {
+    font: {
+      size: 0.8em;
+    }
+  }
+}
+</style>
