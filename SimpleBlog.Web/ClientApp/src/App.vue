082bc12a025b7169446e958d5ec6f13f78b6ac6c
Jakub <kubasurdej@gmail.com>
2023-01-22 14:03:11 +0100

    Start styling application

--- SimpleBlog.Web/ClientApp/src/App.vue
+++ SimpleBlog.Web/ClientApp/src/App.vue
@@ -1,15 +1,52 @@
 <template>
-  <v-app>
-    <v-app-bar title="Application"></v-app-bar>
+  <v-app :theme="theme">
+    <v-app-bar title="SimpleBlog" color="primary" prominent>
+      <v-app-bar-nav-icon
+        variant="text"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
 
-    <v-navigation-drawer
-      ><router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link></v-navigation-drawer
-    >
+      <v-spacer></v-spacer>
+
+      <v-btn :prepend-icon="getThemeIcon()" @click="onClick"
+        >Toggle Theme</v-btn
+      >
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer">
+      <v-list :items="items"></v-list>
+    </v-navigation-drawer>
 
     <v-main><router-view /></v-main>
   </v-app>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const theme = ref("light");
+
+const onClick = () => {
+  theme.value = theme.value === "light" ? "dark" : "light";
+};
+
+const getThemeIcon = () => {
+  return theme.value === "light" ? "mdi-weather-sunny" : "mdi-weather-night";
+};
+</script>
+
+<script lang="ts">
+export default {
+  data: () => ({
+    drawer: false,
+    group: null,
+    items: [
+      { title: "Home", value: "home" },
+      { title: "About", value: "about" },
+    ],
+  }),
+};
+</script>
 
 <style lang="scss">
 @import "./styles/index.scss";
