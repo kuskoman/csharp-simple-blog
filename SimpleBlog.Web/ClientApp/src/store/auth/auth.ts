d3bd6192cc9217eb62dca91e19df72e02bd5b6a5
Jakub <jakub.surdej@codahead.com>
2023-01-23 20:22:37 +0100

    Add register action to auth store

--- SimpleBlog.Web/ClientApp/src/store/auth/auth.ts
+++ SimpleBlog.Web/ClientApp/src/store/auth/auth.ts
@@ -1,5 +1,6 @@
 import { ActionContext, ActionTree, Module } from "vuex";
 import { ApiClient } from "../../api/ApiClient";
+import { ALERT_ACTION_TYPES, ALERT_STORE, ALERT_TYPE, ApplicationAlert } from "../alert";
 import { AUTH_ACTION_TYPES } from "./auth.action-types";
 import { AuthState, RegisterParams } from "./auth.interfaces";
 
@@ -12,13 +13,30 @@ const getters = {};
 const mutations = {};
 
 const actions: ActionTree<AuthState, AuthState> = {
-  [AUTH_ACTION_TYPES.REGISTER]: async (
-    { commit, state }: ActionContext<AuthState, AuthState>,
-    registerParams: RegisterParams
-  ) => {
+  [AUTH_ACTION_TYPES.REGISTER]: async (ctx: ActionContext<AuthState, AuthState>, registerParams: RegisterParams) => {
+    const { dispatch } = ctx;
     const authResponse = await ApiClient.postAuthSignup({
       body: registerParams,
     });
+
+    const addAlertAction = `${ALERT_STORE}/${ALERT_ACTION_TYPES.ADD_ALERT}`;
+
+    if (authResponse.status !== 201) {
+      const failureAlert: ApplicationAlert = {
+        type: ALERT_TYPE.ERROR,
+        title: "Login failed",
+        body: (authResponse.body.errors || []).map((e) => e.description).join("\n"),
+      };
+      await dispatch(addAlertAction, failureAlert, { root: true });
+      return;
+    }
+
+    const registerSuccessfulAlert: ApplicationAlert = {
+      type: ALERT_TYPE.SUCCESS,
+      title: "Welcome!",
+      body: "Successfully created an account",
+    };
+    await dispatch(addAlertAction, registerSuccessfulAlert, { root: true });
   },
 };
 
