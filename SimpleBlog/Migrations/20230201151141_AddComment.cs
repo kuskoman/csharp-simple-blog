3036a95527b9cf7e0cb0f3b05460b90be832b35f
Jakub <jakub.surdej@codahead.com>
2023-02-01 16:11:55 +0100

    Add comments to database

---
+++ SimpleBlog/Migrations/20230201151141_AddComment.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SimpleBlog.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "Posts",
+                newName: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Posts",
+                newName: "id");
+        }
+    }
+}
