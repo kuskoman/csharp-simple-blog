3e10e274434b6dbe8c98f2362abf3ee00b69d4e1
Jakub <kubasurdej@gmail.com>
2023-02-04 15:54:05 +0100

    Add button for adding new post

---
+++ SimpleBlog.Web/ClientApp/src/components/Blog/NewBlogButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-btn icon color="info" class="new-blog-btn" size="large" elevation="2" fab @click="onClick">
+    <v-icon>mdi-plus</v-icon>
+  </v-btn>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+const router = useRouter();
+
+const onClick = () => {
+  router.push("/posts/new");
+};
+</script>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NewBlogButton",
+});
+</script>
+
+<style scoped lang="scss">
+@import "@/styles/mixins/buttons.scss";
+
+.new-blog-btn {
+  @include fab;
+}
+</style>
--- SimpleBlog.Web/ClientApp/src/views/Blog/BlogView.vue
+++ SimpleBlog.Web/ClientApp/src/views/Blog/BlogView.vue
@@ -2,11 +2,13 @@
   <div v-for="post in posts" :key="post.id">
     <post :body="post.content" :author="post.author" :title="post.title"></post>
   </div>
+  <new-blog-button></new-blog-button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 import Post from "@/components/Blog/Post.vue";
+import NewBlogButton from "@/components/Blog/NewBlogButton.vue";
 import { PostsClient } from "@/api/PostsClient";
 
 const postsResponse = await PostsClient.postsGet();
@@ -16,6 +18,7 @@ export default defineComponent({
   name: "BlogView",
   components: {
     Post,
+    NewBlogButton,
   },
   data: () => ({ posts }),
 });
